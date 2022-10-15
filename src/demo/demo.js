7f09677ff48ef54fd2301d87beae9e449e78508f
Kaan Ersoy <kaanersoy12@gmail.com>
2022-10-15 13:36:11 +0300

    update demo (#4)
    * update demo
    
    * remove console message

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,7 +1,20 @@
+let experienceGained = 0;
+const EXPERIENCE_ELEMENT_QUERY = ".withObserving";
+const experienceBar = document.querySelector(".experience-bar");
+const currentExperience = document.querySelector(".current-experience");
+const allExperienceElements = document.querySelectorAll(EXPERIENCE_ELEMENT_QUERY);
+
+const addExperiences = () => {
+  experienceGained++;
+  const allCount = allExperienceElements.length;
+  const percentage = (100 * experienceGained) / allCount;
+  currentExperience.style.transform = `translateY(${100 - percentage}%)`;
+};
+
 didISee(
-  ".withObserving",
+  EXPERIENCE_ELEMENT_QUERY,
   ({ target }) => {
-    console.log("we saw it!", target);
+    addExperiences();
   },
   {
     once: true,

bcfa2615dcf90700dc0dd276c53fdd0f452b419d
kaanersoy <kaanersoy12@gmail.com>
2022-10-15 13:33:05 +0300

    update demo

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -1,7 +1,21 @@
+let experienceGained = 0;
+const EXPERIENCE_ELEMENT_QUERY = ".withObserving";
+const experienceBar = document.querySelector(".experience-bar");
+const currentExperience = document.querySelector(".current-experience");
+const allExperienceElements = document.querySelectorAll(EXPERIENCE_ELEMENT_QUERY);
+
+const addExperiences = () => {
+  experienceGained++;
+  const allCount = allExperienceElements.length;
+  const percentage = (100 * experienceGained) / allCount;
+  console.log(100 - percentage);
+  currentExperience.style.transform = `translateY(${100 - percentage}%)`;
+};
+
 didISee(
-  ".withObserving",
+  EXPERIENCE_ELEMENT_QUERY,
   ({ target }) => {
-    console.log("we saw it!", target);
+    addExperiences();
   },
   {
     once: true,
